983283b1bef1e09ee10e82b505ddb36c502c8d64
小七爷 <806707499@qq.com>
2020-04-28 16:28:47 +0800

    新增测试用例sample-test1及环境配置模板

---
+++ PagedLOD/Tests/UnitTests/Sample-Test1/test.cpp
@@ -0,0 +1,37 @@
+#include "pch.h"
+
+#include "MockData.h"
+#include "Utils.h"
+#include <GLM/glm.hpp>
+
+using namespace hivePagedLOD;
+
+TEST(CulllingTest, TestIsBoundingSphereInFrustum)
+{
+	auto TileNodeSet = MockData::getTileNodeSet();
+	auto ViewInfoSet = MockData::getViewInfoSet();
+	std::vector<glm::vec4> FrustumPlanes;
+	std::vector<std::vector<bool>> Expected
+	{ 
+	{false, true, false, true, false}, 
+	{true, false, false, true, false}, 
+	{false, true, true, true, false} 
+	};
+
+	for (unsigned int i = 0; i < ViewInfoSet.size(); i++)
+	{
+		glm::mat4 ViewMat, ProjectionMat;
+
+		CUtils::getInstance()->calculateMatrices(ViewMat, ProjectionMat, ViewInfoSet[i]);
+		FrustumPlanes.clear();
+		CUtils::getInstance()->calculateFrustumPlane(FrustumPlanes, ViewInfoSet[i]);
+
+		for (unsigned int j = 0; j < TileNodeSet.size(); ++j)
+		{
+			SCOPED_TRACE("row:" + std::to_string(i) + "\tcol:" + std::to_string(j));
+			glm::vec4 Center = ViewMat * glm::vec4(TileNodeSet[j]->getBoundingSphere().getCenter(), 1.0f);
+			float Radius = TileNodeSet[j]->getBoundingSphere().getRadius();
+			EXPECT_EQ(Expected[i][j], CUtils::getInstance()->isBoundingSphereInFrustum(FrustumPlanes, Center, Radius));
+		}
+	}
+}
